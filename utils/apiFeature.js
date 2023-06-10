b9db0775e6d3d945463a352a5490ba61a96247cb
Shobhit Kushwaha <shobhitkushwaha1406@gmail.com>
2023-06-10 15:03:14 +0530

    wallet bug fixed

--- utils/apiFeature.js
+++ utils/apiFeature.js
@@ -21,7 +21,7 @@ export const ChechIfWalletConnected = async () => {
 export const connectWallet = async () => {
     try {
 
-
+        git
         if (!window.ethereum) return console.log("Install MetaMask");
 
         const accounts = await window.ethereum.request({
